085fee6c6ebb8eb0339c607f493452938f198f8f
CrusadeTX <angel.terziev97@gmail.com>
2020-11-11 17:03:20 +0200

    added quote filtering by category

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,24 @@
+/// <reference path="../typings/tsd.d.ts" /
 $(document).ready(function(){
-    var limit = $('input[name="limit"]:checked').val();
     var apiResponse ="";
+    var requestComplete= false;
+    var templateList =[];
 
 
-    function doApiRequest(type, fname, lname){
-        var limit = $('input[name="limit"]:checked').val();
+    function doApiRequest(type){       
+        $.ajax({
+            method: "GET",
+           // url: "https://api.icndb.com/jokes/" + apiType + apiLimit + apiName,
+           url: QueryBuilder(type),
+           dataType: "json"
+        }).done(function(response){
+           console.log(response);
+           console.log(templateList);
+            getAllQuotes(response);
+        }).fail(function(){
+            console.log("Something went wrong")
+        })
+       /* var limit = $('input[name="limit"]:checked').val();
         var apiType="";
         var apiName ="";
         var apiLimit="";
@@ -38,25 +52,19 @@ $(document).ready(function(){
             else{
                 apiName="";
             }
-        }
-
-    $.ajax({
-        method: "GET",
-       // url: "https://api.icndb.com/jokes/" + apiType + apiLimit + apiName,
-       url: "https://api.icndb.com/jokes/",
-       dataType: "json"
-    }).done(function(response){
-        console.log(response);
-        getAllQuotes(response);
-    }).fail(function(){
-        console.log("Something went wrong")
-    })
+        }*/
 }
 
-function getAllQuotes(data){
 
+function getApiResponse(response){
+return response;
+}
+
+
+
+function getAllQuotes(data){
+    clearAllQuotes()
     for (i=0;i<data.value.length;i++){
- 
     var template = $('#card-template').clone();
     template.attr('id', '');
     console.log(template.find('span').text(data.value[i].joke))
@@ -64,7 +72,13 @@ function getAllQuotes(data){
     generateImage(img);
     $('#quote-container').append(template);
     template.show();
+    templateList.push(template);
     }
+}
+
+function clearAllQuotes(){
+ $("#quote-container").empty();
+
 }
 function getPersonalisedQuote(data){
 
@@ -91,6 +105,47 @@ function getRandomInt(max) {
     return 1+Math.floor(Math.random() * Math.floor(max));
   }
 
-doApiRequest('random','Angel','Terziev')
-console.log(apiResponse);
+$('input[name="limit"]').click(function(){
+  doApiRequest("")
+})
+
+function QueryBuilder(type){
+    var fname;
+    var lname;
+    var limit;
+    var creds;
+    var apiLimit;
+    var apiType;
+
+    if(type === "random"){
+    apiType ="random";
+    fname = $("#first-name").val();
+    lname = $("#last-name").val();
+    if(fname !=="" && lname !==""){
+        creds = "firstName=" + fname + "&lastName=" + lname;
+    }
+    }
+    else{
+    apiType="";
+    creds="";;
+    }
+   var limit = $('input[name="limit"]:checked').val();
+    switch(limit){
+        case "nerdy": apiLimit ="?limitTo=[nerdy]"; break;
+        case "explicit": apiLimit ="?limitTo=[explicit]"; break;
+        case "all": apiLimit ="";
+        if(fname !=="" && lname !==""&& type==="random"){
+            creds = "?firstName=" + fname + "&lastName=" + lname;
+        }; break;
+        default: apiLimit =""; break;
+    }
+
+    var query = "https://api.icndb.com/jokes/"+apiType+apiLimit+creds;
+    debugger;
+    return query;
+
+
+
+
+}
 })
