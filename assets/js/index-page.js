9b33e2e560f01d8ea2f648595bed9b7973ac972a
CrusadeTX <angel.terziev97@gmail.com>
2020-11-17 19:28:14 +0200

    added basic validation to the registration page

--- assets/js/index-page.js
+++ assets/js/index-page.js
@@ -5,10 +5,12 @@ $('#registration-row').attr( "style", "display: none !important;" );
 $('#register').on('click', function(){
     $('#log-in-row').attr( "style", "display: none !important;" );
     $('#registration-row').attr( "style", "display: flex !important;" );
-    console.log(1)
 });
 $('#btn-back').click(function(){
     $('#log-in-row').attr( "style", "display: flex !important;" );
     $('#registration-row').attr( "style", "display: none !important;" );
 })
+
+
+
 })
